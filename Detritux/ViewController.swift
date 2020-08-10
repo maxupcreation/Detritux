b267ca9c6a2eba7ce1faa8c0c7c9d02510432b57
Maxime <maxupcreation@gmail.com>
2020-08-10 13:33:46 +0200

    Initial Commit

---
+++ Detritux/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Detritux
+//
+//  Created by Maxime on 10/08/2020.
+//  Copyright © 2020 Maxime. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
